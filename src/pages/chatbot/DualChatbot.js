38ccd5f8cd56c019655321025c2cd53f7587b44a
milisheta7806 <shetamili7806@gmail.com>
2025-08-23 12:28:29 +0530

    Update DualChatbot.js

--- src/pages/chatbot/DualChatbot.js
+++ src/pages/chatbot/DualChatbot.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import DeveloperChat from './DeveloperChat';
 import './DualChatbot.css';
 
 const DualChatbot = () => {
+  const location = useLocation();
+  const projectInfo = location.state || {};
+
   return (
     <div className="dual-chatbot-container">
       <div className="chatbot-content">
-        <DeveloperChat />
+        <DeveloperChat projectInfo={projectInfo} />
       </div>
     </div>
   );
